4a0fcfa783d2d0dfedba55f14055381e3c06b457
jggomeztocinouca <jesus.gomeztocino@alum.uca.es>
2023-03-21 12:06:06 +0100

    Pequeña correción del tercer ejercicio de Nodos Verdes y comienzo de la práctica 3.

---
+++ Practica3/Ejercicio1.cpp
@@ -0,0 +1,19 @@
+#include "../Agen/agenCeldasEnlazadas.cpp"
+
+// Ejercicio 1: Implementa un subprograma que dado un árbol general nos calcule su grado. El grado de un árbol es el máximo número de hijos que tiene un nodo.
+
+template <typename T>
+int gradoRec(const typename Agen<T>::nodo n, const Agen<T>& A){
+    if (n == Agen<T>::NODO_NULO){
+        return 0;
+    }
+    else{
+        typename Agen<T>::nodo h = A.hijoIzqdo(n);
+        int contador = 0;
+        while(h != Agen<T>::NODO_NULO){
+            contador++;
+            h = A.hermDrcho(h);
+        }
+        return max(contador,max(gradoRec(A.hijoIzqdo(n),A), gradoRec(A.hermDrcho(n),A)));
+    }
+}
