b08946a05da3a8c185ec872b4ffec2650b1ed64a
Jesús Gómez <jesus.gomeztocino@alum.uca.es>
2023-04-20 16:56:28 +0200

    Revisión de la Práctica 4 terminada.

--- Rev1/Practica 3/Ejercicio 4/Ejercicio4.cpp
+++ Rev1/Practica 3/Ejercicio 4/Ejercicio4.cpp
@@ -1,15 +1,54 @@
 #include <Agen.hpp>
 
-template<typename T>
-const typename Agen<T>::nodo (const typename Agen<T>::nodo& n, const Agen<T>& A)
+typename Agen<int>::nodo busquedaNodoRec(const typename Agen<int>::nodo& n, int x, const Agen<int>& A)
 {
-    if(n == Agen<T>::NODO_NULO)
+    if(n == Agen<int>::NODO_NULO)
+    {
+        return Agen<int>::NODO_NULO;
+    }
+    else if(A.elemento(n) == x)
+    {
+        return n;
+    }
+    else
+    {
+        typename Agen<int>::nodo nodoBuscar = Agen<int>::NODO_NULO;
+        typename Agen<int>::nodo hijo = A.hijoIzqdo(n);
+        while(hijo != Agen<int>::NODO_NULO)
+        {
+            nodoBuscar = busquedaNodoRec(hijo, x, A);
+            hijo = A.hermDrcho(n);
+        }
+        return nodoBuscar;
+    }
 }
 
-template<typename T>
-void podaNodoRec(const typename Agen<T>::nodo& n, const Agen<T>& A)
+typename Agen<int>::nodo busquedaNodo(int x, const Agen<int>& A)
 {
-    if(n != Agen<T>::NODO_NULO){
+    return busquedaNodoRec(A.raiz(), x, A);
+}
 
+void podaNodoRec(const typename Agen<int>::nodo& n, Agen<int>& A)
+{
+    if(n != Agen<int>::NODO_NULO)
+    {
+        typename Agen<int>::nodo hijo = A.hijoIzqdo(n);
+        while(hijo != Agen<int>::NODO_NULO)
+        {
+            podaNodoRec(hijo,A);
+            A.eliminarHijoIzqdo(n);
+            hijo = A.hermDrcho(hijo);
+        }
+    }
+}
+
+void podaNodo(int x, Agen<int>& A)
+{
+    if(!A.arbolVacio()) {
+        typename Agen<int>::nodo nodoPodar = busquedaNodo(x,A);
+        if(nodoPodar != Agen<int>::NODO_NULO)
+        {
+            podaNodoRec(nodoPodar, A);
+        }
     }
 }
