32d3158fdf40fe398da3c46bda5d9116dfbabc67
Jesús Gómez <jesus.gomeztocino@alum.uca.es>
2023-04-29 18:58:16 +0200

    Comienzo de la práctica 6

--- Practica6/Ejercicio 1/Ejercicio.cpp
+++ Practica6/Ejercicio 1/Ejercicio.cpp
@@ -1,1 +1,22 @@
+#include <alg_grafoPMC.h>
 
+/**
+ * @brief Algoritmo de Dijkstra Inverso \n
+ * Si el algoritmo de Dijkstra calcula los costes mínimos entre origen y todos los vértices,
+ * el algoritmo inverso calculará los costes mínimos entre un destino común y todos los vértices.
+ *
+ * @tparam tCoste Tipo de dato asociado al coste de los arcos.
+ * @param G Grafo ponderado. Asocia a cada vértice i del grafo una lista que almacena todos los vértices adyacentes a i.
+ * @param destino Nodo destino.
+ * @param P Vector de vértices tal que P[i] es el vértice anterior a i en el camino de coste mínimo desde origen hasta i.
+ * @return Costes mínimos entre un destino común y todos los vértices
+ */
+
+template <typename tCoste>
+vector<tCoste> DijkstraInv(const GrafoP<tCoste>& G, typename GrafoP<tCoste>::vertice destino, vector<typename GrafoP<tCoste>::vertice>& P)
+{
+    typedef typename GrafoP<tCoste>::vertice vertice; // Para ahorrarnos escribir typename GrafoP<tCoste>::vertice cada vez que queramos usarlo.
+    vertice v, w;
+    const size_t n = G.numVert(); // Número de vertices en el grafo ponderado
+
+}
--- Practica6/Ejercicio 2/Ejercicio.cpp
+++ Practica6/Ejercicio 2/Ejercicio.cpp
@@ -1,3 +1,41 @@
-//
-// Created by jggomeztocino on 25/04/2023.
-//
+#include <alg_grafoPMC.h>
+
+template<typename tCoste>
+tCoste diametroPseudocentro(const GrafoP<tCoste>& G)
+{
+    typedef typename GrafoP<tCoste>::vertice vertice; // Para ahorraros escribir typename GrafoP<tCoste>::vertice
+    const size_t n = G.numVert(); // Número de vértices del grafo
+
+    matriz<vertice> P(n); // Matriz vacía donde se almacenará el camino mínimo a cada vértice
+    matriz<tCoste> costes = Floyd(G,P); // Matriz de costes mínimos
+
+    tCoste diametro = GrafoP<tCoste>::INFINITO;
+    tCoste diametroActual = 0;
+    tCoste mayor, mayor2;
+
+    for(size_t i = 0; i < n-1; i++)
+    {
+        mayor = 0;
+        mayor2 = 0;
+        for(size_t j = 0; j < n-1; j++)
+        {
+            if(costes[i][j] > mayor) // No comprobamos el caso de infinito, porque es un grafo conexo
+            {
+                mayor2 = mayor;
+                mayor = costes[i][j];
+            }
+            else if(costes[i][j] > mayor2)
+            {
+                mayor2 = costes[i][j];
+            }
+        }
+        diametroActual = suma(mayor, mayor2);
+        if(diametro >= diametroActual)
+        {
+            diametro = diametroActual;
+        }
+    }
+
+    return diametro;
+
+}
