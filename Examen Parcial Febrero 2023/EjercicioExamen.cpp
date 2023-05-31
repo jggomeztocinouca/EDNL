ed9da57fb2045b702c70b8d568ceb3b7e7bb8fd7
Jesús Gómez <jesus.gomeztocino@alum.uca.es>
2023-05-31 21:36:44 +0200

    2 :(

--- Examen Parcial Febrero 2023/EjercicioExamen.cpp
+++ Examen Parcial Febrero 2023/EjercicioExamen.cpp
@@ -44,3 +44,52 @@ bool similares(const Agen<T>& A, const Agen<T>& B)
 {
     return similares(A.raiz(), B.raiz(), A, B);
 }
+
+#include <iostream>
+#include <cassert>
+
+void test_similares() {
+    // Caso 1: Ambos árboles están vacíos.
+    Agen<int> A1, B1;
+    assert(similares(A1, B1) == true);
+
+    // Caso 2: Un árbol está vacío y el otro no.
+    Agen<int> A2, B2;
+    A2.insertarRaiz(1);
+    assert(similares(A2, B2) == false);
+
+    // Caso 3: Ambos árboles contienen solo un nodo (la raíz).
+    Agen<int> A3, B3;
+    A3.insertarRaiz(1);
+    B3.insertarRaiz(1);
+    assert(similares(A3, B3) == true);
+
+    // Caso 4: Los árboles tienen la misma estructura pero diferentes elementos.
+    Agen<int> A4, B4;
+    A4.insertarRaiz(1);
+    B4.insertarRaiz(2);
+    assert(similares(A4, B4) == false);
+
+    // Caso 5: Los árboles tienen la misma estructura y los mismos elementos.
+    Agen<int> A5, B5;
+    A5.insertarRaiz(1);
+    A5.insertarHijoIzqdo(A5.raiz(), 2);
+    B5.insertarRaiz(1);
+    B5.insertarHijoIzqdo(B5.raiz(), 2);
+    assert(similares(A5, B5) == true);
+
+    // Caso 6: Los árboles tienen diferentes estructuras pero algunos elementos en común.
+    Agen<int> A6, B6;
+    A6.insertarRaiz(1);
+    A6.insertarHijoIzqdo(A6.raiz(), 2);
+    B6.insertarRaiz(1);
+    assert(similares(A6, B6) == false);
+
+    std::cout << "Todos los tests pasaron correctamente." << std::endl;
+}
+
+int main() {
+    test_similares();
+    return 0;
+}
+
